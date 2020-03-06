ab97df9bf19185c9bbdc4f01af6c7140f202e938
Stefan Wenk <stefan.wenk@celsi.ch>
2020-03-06 16:36:01 +0100

    parameterized data wrapper with T

--- src/main/java/ch/wenkst/sw_utils/miscellaneous/DataWrapper.java
+++ src/main/java/ch/wenkst/sw_utils/miscellaneous/DataWrapper.java
@@ -6,17 +6,19 @@ public class DataWrapper {
 	/**
 	 * simple wrapper class around an object. the idea is to wrap any object in
 	 * a class to get some advantages of classes
+	 * @param <T>
 	 * @param data	the object that is held by the Data Wrapper
 	 */
-	public DataWrapper(Object data) {
+	public <T> DataWrapper(T data) {
 		this.data = data;
 	}
 	
-	public Object getData() {
-		return data;
+	@SuppressWarnings("unchecked")
+	public <T> T getData() {
+		return (T) data;
 	}
 
-	public void setData(Object data) {
+	public <T> void setData(T data) {
 		this.data = data;
 	}
 }
